cf6090f637f114898a75b4a8b65f4425b04431f9
Lera Sysenko <lerasysenkoqwer@gmail.com>
2026-02-16 03:16:40 +0100

    add js-7

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,13 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-const inputValue = (event)=>{
- if(event.currentTarget.value === "" || event.currentTarget.value === "    "){
-    output.textContent = "Anonymous";
- }
- output.textContent = event.currentTarget.value.trim();
-    
+const inputValue = (event) => {
+   const value = event.currentTarget.value.trim();
+   if (value === "") {
+      output.textContent = "Anonymous";
+   } else {
+      output.textContent = value;
+      
+   }
 };
 input.addEventListener("input",inputValue);
--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,8 @@
 const form = document.querySelector(".js-login-form");
 const onFormHandler = (event)=> {
     event.preventDefault();
-    const email = event.target.elements.email.value;
-    const password = event.target.elements.password.value;
+    const email = event.target.elements.email.value.trim();
+    const password = event.target.elements.password.value.trim();
     if(email === "" || password ===""){
         alert ( "All form fields must be filled in");
     }
